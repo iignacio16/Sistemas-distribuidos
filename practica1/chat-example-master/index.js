76045731fc4dece42453db01f202e5d596131bde
Ignacio González Del Pozo <ignacio@MacBook-Pro-de-Ignacio-2.local>
2022-03-01 20:59:27 +0100

    practica terminada

--- practica1/chat-example-master/index.js
+++ practica1/chat-example-master/index.js
@@ -1,18 +1,21 @@
-const app = require('express')();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
-const port = process.env.PORT || 3000;
+const app = require('express')()
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
+const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
+    res.sendFile(__dirname + '/index.html')
+})
 
 io.on('connection', (socket) => {
-  socket.on('chat message', msg => {
-    io.emit('chat message', msg);
-  });
-});
+    console.log('new connection', socket.id)
+
+    socket.on('chat message', (msg) => {
+        //escucha los datos enviados
+        io.emit('chat message', msg)
+    })
+})
 
 http.listen(port, () => {
-  console.log(`Socket.IO server running at http://localhost:${port}/`);
-});
+    console.log(`Socket.IO server running at http://localhost:${port}/`)
+})
